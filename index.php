a10862722ad18108957d0ec1be3e28d6e31d25b7
Dora Budić <dbudic0706@gmail.com>
2021-04-30 15:44:55 +0200

    Header

--- index.php
+++ index.php
@@ -9,9 +9,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP Google FAQ</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./dist/master.css">
   </head>
   <body>
+    <header>
+      <div class="page-title">
+        <div class="logo">
+          <a href="#"><img src="./dist/img/logo.svg" alt="google logo"></a>
+          <a href="#"><span>Privacy &amp; Terms</span></a>
+        </div>
+        <div class="icons">
+          <a href="#">
+            <svg>
+              <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+            </svg>
+          </a>
+          <a href="#"><img src="./dist/img/account.png" alt="account logo"></a>
+        </div>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Overview</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Terms of Service</a></li>
+          <li><a href="#">Technologies</a></li>
+          <li class="active"><a href="#">FAQ</a></li>
+        </ul>
+      </nav>
+    </header>
 
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
   </body>
 </html>
